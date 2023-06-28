3ed1aaa76ec4f74d4d9ebaf41c82562ada301be8
Neeraj singh saini <116188549+neeraj2612@users.noreply.github.com>
2023-06-29 02:53:44 +0530

    Delete src directory

--- src/Components/Students.js
+++
@@ -1,8 +0,0 @@
-const Students = [
-    { id: 1, name: 'John', grade: 'A' },
-    { id: 2, name: 'Jane', grade: 'B' },
-    // Add more student objects as needed
-  ];
-  
-  export default Students;
-  
